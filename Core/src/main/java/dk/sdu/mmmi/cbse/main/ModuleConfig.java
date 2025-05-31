8bd5a997f4d4f11f6b60d5f6691a5e1da378afde
Patrick <patrick.hougesen@gmail.com>
2025-05-31 11:20:05 +0200

    Split issue fix

--- Core/src/main/java/dk/sdu/mmmi/cbse/main/ModuleConfig.java
+++ Core/src/main/java/dk/sdu/mmmi/cbse/main/ModuleConfig.java
@@ -3,36 +3,43 @@ package dk.sdu.mmmi.cbse.main;
 import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
 import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
 import dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
-import dk.sdu.mmmi.cbse.common.util.ServiceLocator;
-import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 @Configuration
 class ModuleConfig {
-    
+
     public ModuleConfig() {
     }
 
     @Bean
-    public Game game() {
-        return new Game();
-    }
-
-    @Bean
-    public List<IEntityProcessingService> entityProcessingServiceList() {
-        return ServiceLocator.INSTANCE.locateAll(IEntityProcessingService.class);
+    public Game game(List<IGamePluginService> gamePluginServices,
+                     List<IEntityProcessingService> entityProcessingServices,
+                     List<IPostEntityProcessingService> postEntityProcessingServices) {
+        return new Game(gamePluginServices, entityProcessingServices, postEntityProcessingServices);
     }
 
     @Bean
     public List<IGamePluginService> gamePluginServices() {
-        return ServiceLocator.INSTANCE.locateAll(IGamePluginService.class).stream()
-                .filter(plugin -> !plugin.getClass().getModule().getName().equals("enemysystem"))
-                .toList();
+        // Load all discovered plugin modules from the /plugins folder
+        Set<String> moduleNames = PluginLoader.discoverModules("plugins");
+        return PluginLoader.loadPlugins("plugins", moduleNames, IGamePluginService.class);
+    }
+
+    @Bean
+    public List<IEntityProcessingService> entityProcessingServiceList() {
+        Set<String> moduleNames = PluginLoader.discoverModules("plugins");
+        return PluginLoader.loadPlugins("plugins", moduleNames, IEntityProcessingService.class);
     }
 
     @Bean
     public List<IPostEntityProcessingService> postEntityProcessingServices() {
-        return ServiceLocator.INSTANCE.locateAll(IPostEntityProcessingService.class);
+        Set<String> moduleNames = PluginLoader.discoverModules("plugins");
+        return PluginLoader.loadPlugins("plugins", moduleNames, IPostEntityProcessingService.class);
     }
 }
